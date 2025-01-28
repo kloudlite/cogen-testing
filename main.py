f846dc097818c7910e5d813a53bbc8627e770458
nxtcoder36 <mohit0698@gmail.com>
2025-01-28 17:19:02 +0530

    wip

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from agentman import tool, action
 
-@tool(
-  "gmail",
-  description="Tool to send emails", 
-  parameters={"email": "Email address"},
-  required=["email"],
-  category="communication"
-)
 class GmailTool:
-  def __init__(self):
-      return self
 
   @action(
     "send_email",
@@ -18,8 +9,7 @@ class GmailTool:
       "to": {"type": "string", "description": "Email address to send the email to"},
       "subject": {"type": "string", "description": "Subject of the email"},
       "body": {"type": "string", "description": "Body of the email"}
-    },
-    required=["subject", "body"]
+    }
   )
   def send_email(self, to: str, subject: str, body: str):
       print(f"Sending email to {to} with subject: {subject} and body: {body}")
